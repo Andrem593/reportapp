b8a73a118e880421fd67f3d11f5991b7b09b3ba9
Andres Mendoza <andres.mendoza@svtechnology.ec>
2023-02-22 10:32:04 -0500

    arreglo de rutas

--- resources/js/src/router.js
+++ resources/js/src/router.js
@@ -139,7 +139,7 @@ router.beforeEach((to, from, next) => {
             next({ name: "login" });
         }
     } else {
-        if (from.name == "login" && localStorage.getItem("token") != null || from.name == null && localStorage.getItem("token") != null) {
+        if (from.name == "login" && localStorage.getItem("token") != null) {
             next({ name: "home" });
         } else {
             next();
