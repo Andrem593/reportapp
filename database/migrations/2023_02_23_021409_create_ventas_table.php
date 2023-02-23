49bbf2fb5686d356162e3ae8a1b12672c7d99362
Christian Ramos <crismas06@outlook.com>
2023-02-23 16:51:14 -0500

    model clientes

--- database/migrations/2023_02_23_021409_create_ventas_table.php
+++ database/migrations/2023_02_23_021409_create_ventas_table.php
@@ -15,6 +15,65 @@ class CreateVentasTable extends Migration
     {
         Schema::create('ventas', function (Blueprint $table) {
             $table->id();
+
+            $table->integer("tienda_id");
+            $table->integer("cliente_id");
+            $table->string("division");
+            $table->string("departamento");
+            $table->integer("producto_id");
+            $table->integer("descuento_actual");
+            $table->float("pvp_factura", 8, 2);
+            $table->float("pvp", 8, 2);
+            $table->float("pvp_mayorista", 8, 2);
+            $table->string("punto_operacion");
+            $table->date("fecha");
+            $table->time("hora");
+            $table->string("vendedor");
+            $table->string("cajero");
+            $table->string("tipo");
+            $table->string("transaccion");
+            $table->integer("cantidad");
+            $table->float("costo", 8, 2);
+            $table->float("subtotal", 8, 2);
+            $table->integer("descuento_manual");
+            $table->integer("descuento_sistema");
+            $table->integer("descuento");
+            $table->float("venta_neta", 8, 2);
+            $table->float("impuesto", 8, 2);
+            $table->float("total", 8, 2);
+            $table->float("utilidad", 8, 2);
+            $table->integer("gm");
+            $table->float("efectivo", 8, 2);
+            $table->float("tarjeta_credito", 8, 2);
+            $table->float("tarjeta_debito", 8, 2);
+            $table->float("nota_credito", 8, 2);
+            $table->float("cupo_credito", 8, 2);
+            $table->float("cheque", 8, 2);
+            $table->float("otros", 8, 2);            
+            $table->float("devolucion", 8, 2);            
+            $table->float("medianet", 8, 2);            
+            $table->float("datafast", 8, 2);            
+            $table->float("austro", 8, 2);            
+            $table->float("visa", 8, 2);            
+            $table->float("mastercard", 8, 2);            
+            $table->float("american_express", 8, 2);            
+            $table->float("diners", 8, 2);            
+            $table->float("discovery", 8, 2);            
+            $table->float("pacificard", 8, 2);            
+            $table->float("bankcard", 8, 2);            
+            $table->float("alia", 8, 2);            
+            $table->float("otras_tarjetas", 8, 2);            
+            $table->float("banco_pichincha", 8, 2);            
+            $table->float("banco_pacifico", 8, 2);            
+            $table->float("banco_guayaquil", 8, 2);            
+            $table->float("banco_bolivariano", 8, 2);            
+            $table->float("banco_produbanco", 8, 2);            
+            $table->float("banco_austro", 8, 2);            
+            $table->float("banco_internacional", 8, 2);            
+            $table->float("banco_solidario", 8, 2);            
+            $table->float("banco_machala", 8, 2);            
+            $table->float("otros_bancos", 8, 2);
+            
             $table->timestamps();
         });
     }
