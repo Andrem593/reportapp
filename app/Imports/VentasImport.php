f46489c0534f111d8f0faa625b6c55afa4080014
Christian Ramos <crismas06@outlook.com>
2023-03-02 15:52:49 -0500

    model clientes

--- app/Imports/VentasImport.php
+++ app/Imports/VentasImport.php
@@ -2,6 +2,8 @@
 
 namespace App\Imports;
 
+use App\Models\Producto;
+use App\Models\tienda;
 use App\Models\Venta;
 use Maatwebsite\Excel\Concerns\ToModel;
 
@@ -12,10 +14,28 @@ class VentasImport implements ToModel
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+
+    private $asignacion;
+
+    public function __construct($asignacion)
+    {
+        $this->asignacion = $asignacion;
+    }
+
     public function model(array $row)
     {
-        return new Venta([
-            //
-        ]);
+        dd($row['tienda']);
+        // $tienda = tienda::where('tienda', $row['tienda'])->where('ciudad', $row['ciudad'])->first()->id;
+        // if (!$tienda) {
+        //     $create = tienda::create([
+        //         'tienda' => $row['tienda'],
+        //         'ciudad' => $row['ciudad']
+        //     ]);
+        //     $tienda = $create->id;
+        // }
+
+        // return new Venta([
+        //     'tienda_id'     => $tienda,
+        // ]);
     }
 }
